c76230834aab50e0e777a59aa7027faf5074c997
TGFC099 <tgfc099@tgfc.tw>
2020-02-17 10:59:52 +0800

    與plugin 在config 上運作的方式, 自己添加plugin 的方法

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ConsoleLogOnBuildWebpackPlugin = require('./ConsoleLogOnBuildWebpackPlugin');
 const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({ title: 'pluginTest' }),
+        new ConsoleLogOnBuildWebpackPlugin()
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
 }
